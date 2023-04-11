42fd522dac075353fc1e763e9d68d8c24f321d82
Mayur Suresh <mayursuresh2022@gmail.com>
2023-04-11 17:11:25 -0400

    annoying changes

--- back-end/app.py
+++ back-end/app.py
@@ -1,51 +1,19 @@
-from flask import Flask, jsonify
-import praw
-import json
-
-client_id = "bG7tZ2k68opbl9agjUeqgg" 
-client_secret = "nkHxd3nTPezesMzUB2AbG56e0FT2rg" 
-user_agent = "maysur_sourcescan"
-
-
-red = praw.Reddit(client_id = client_id, client_secret = client_secret, user_agent = user_agent)
-user_inp =  'trump'  #input('Give Keyword:')
-sub_names = ['news', 'worldnews', 'politics', 'technews']
-articles = []
-
-for i in sub_names:
-    r_sub = red.subreddit(i)
-    for j in r_sub.search(user_inp, sort='top', time_filter='week', limit = 3):
-        article = {
-            'subreddit': 'r/'+i,
-            'title': j.title,
-            'score': j.score,
-            'url': j.url
-        }
-        articles.append(article) 
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
-#CORS(app)
 
-@app.route("/point")
-def data():  
+@app.route('/api/data', methods=['POST', 'GET'])
+def handle_data():
 
-    for i in sub_names:
-        r_sub = red.subreddit(i)
-        for j in r_sub.search(user_inp, sort='top', time_filter='week', limit = 3):
-            article = {
-                'subreddit': 'r/'+i,
-                'title': j.title,
-                'score': j.score,
-                'url': j.url
-            }
-            articles.append(article)
-
-            
-    return jsonify(articles)
-
-    #return jsonify(type(my_dict))
-
+    if request.method == 'POST':
+        data = request.get_json()
+        print(data)
+        # do something with the data
+        response = {'success': True}
+        return jsonify(response)
+    else: 
+        return "not allowed"
+    
 
 if __name__== "__main__":
     app.run(debug = True)
-
--- playbox/sample.py
+++ playbox/sample.py
@@ -31,3 +31,64 @@ def write_list_to_file(file_path, lst):
 write_list_to_file('first.txt', articles)
 
 
+'''
+from flask import Flask, jsonify, request
+import praw
+import json
+
+client_id = "bG7tZ2k68opbl9agjUeqgg" 
+client_secret = "nkHxd3nTPezesMzUB2AbG56e0FT2rg" 
+user_agent = "maysur_sourcescan"
+
+
+red = praw.Reddit(client_id = client_id, client_secret = client_secret, user_agent = user_agent)
+user_inp =  'trump'  #input('Give Keyword:')
+sub_names = ['news', 'worldnews', 'politics', 'technews']
+articles = []
+
+for i in sub_names:
+    r_sub = red.subreddit(i)
+    for j in r_sub.search(user_inp, sort='top', time_filter='week', limit = 3):
+        article = {
+            'subreddit': 'r/'+i,
+            'title': j.title,
+            'score': j.score,
+            'url': j.url
+        }
+        articles.append(article) 
+
+app = Flask(__name__)
+#CORS(app)
+
+@app.route("/point", methods = ['POST'])
+def handle_state_var(): 
+    data = request.json
+    state_variable = data['stateVariable']
+    return state_variable
+
+
+def data():  
+
+    for i in sub_names:
+        r_sub = red.subreddit(i)
+        for j in r_sub.search(user_inp, sort='top', time_filter='week', limit = 3):
+            article = {
+                'subreddit': 'r/'+i,
+                'title': j.title,
+                'score': j.score,
+                'url': j.url
+            }
+            articles.append(article)
+
+            
+    return jsonify(articles)
+
+    #return jsonify(type(my_dict))
+
+
+if __name__== "__main__":
+    app.run(debug = True)
+
+
+
+'''
