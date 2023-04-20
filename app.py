5672cbda6a3148a6f1a1c3e4f92b57365650576f
Mayur Suresh <mayursuresh2022@gmail.com>
2023-04-19 22:43:02 -0400

    database

--- app.py
+++ app.py
@@ -1,8 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, redirect, url_for, session, logging, request
 from data import Article
+from flask_mysqldb import MySQL
+from wtforms import Form, StringField, TextAreaField, PasswordField, validators
+from passlib.hash import sha256_crypt
 
 
 app = Flask(__name__)
+
+# configure MySQL
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'Light@0529202'
+app.config['MYSQL_DB'] = 'soursescan'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+# intialize mysql
+
+mysql = MySQL(app)
+
 Article = Article()
 @app.route('/')
 def index():
@@ -14,10 +28,47 @@ def about():
 @app.route('/data')
 def dataSource():
     return render_template('data.html', articles = Article)
+
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
+class RegisterForm(Form):
+    name = StringField('Name', [validators.Length(min=1, max=50)])
+    username = StringField('Username', [validators.Length(min=4, max=25)])
+    email = StringField('Email', [validators.Length(min=6, max=50)])
+    password = PasswordField('Password', [
+        validators.DataRequired(),
+        validators.EqualTo('confirm', message='Password do not match')
+    ])
+    confirm = PasswordField('Confirm Password')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm(request.form)
+    if request.method == 'POST' and form.validate():
+
+        # mysql stuff
+        name = form.name.data
+        email = form.email.data
+        username = form.username.data
+        password = sha256_crypt.encrypt(str(form.password.data) )# encryption with sha
+        # creating the cursor
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO users(name,email,username,password) VALUES (%s ,%s, %s, %s)", (name, email, username, password))
+
+        #commiting to db
+
+        mysql.connection.commit()
+
+        #close connection
+        cur.close()
+
+        flash(f'You are registered and log in with the username: {username}', 'success')
+
+        return redirect(url_for('index'))
 
 
+    return render_template('register.html', form=form)
 if __name__ == '__main__':
+    app.secret_key='secret_2468'
     app.run(debug=True)
--- data.py
+++ data.py
@@ -4,23 +4,26 @@ client_id = "bG7tZ2k68opbl9agjUeqgg"
 client_secret = "nkHxd3nTPezesMzUB2AbG56e0FT2rg" 
 user_agent = "maysur_sourcescan"
 red = praw.Reddit(client_id = client_id, client_secret = client_secret, user_agent = user_agent)
-user_inp = "Trump"
+user_inp = "Bitcoin"
 
 sub_names = ['news', 'worldnews', 'politics', 'technews']
 articles = []
 
 def Article() :
     for i in sub_names:
+        count = 1
         r_sub = red.subreddit(i)
         for j in r_sub.search(user_inp, sort='top', time_filter='week', limit = 3):
             article = {
+                'id' : count,
                 'subreddit': 'r/'+i,
                 'title': j.title,
                 'score': j.score,
                 'url': j.url
             }
             articles.append(article)
-    print(articles)
+            count+=1
+    #print(articles)
     return articles
 
 Article() 
